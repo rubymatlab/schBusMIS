cff7eaaf1beaeecc3befa664ac5da7ae1a9e2b34
zhyongfeng <rubymatlab@gmail.com>
2019-06-19 23:12:30 +0800

    超出电子围栏微信提醒

--- schBusMIS/src/main/java/org/jeecgframework/web/system/sms/util/task/wxsmsDistanceReminder.java
+++ schBusMIS/src/main/java/org/jeecgframework/web/system/sms/util/task/wxsmsDistanceReminder.java
@@ -56,121 +56,14 @@ public class wxsmsDistanceReminder implements Job {
 	}
 
 	public void run() {
-		System.out.println("执行电子围栏提醒消息开始...");
-
-		List<BasStudentInfoEntity> listBsie = systemService.findByProperty(BasStudentInfoEntity.class, "bsStatus", "Y");
-		List<String> listDeviceid = new ArrayList<String>();
-		List<String> allDeviceid = new ArrayList<String>();
-		String strSize = "";
-		// 分组
-		for (BasStudentInfoEntity t : listBsie) {
-			if (!("".equals(t.getBsDeviceid()) || t.getBsDeviceid() == null)) {
-				allDeviceid.add(t.getBsDeviceid());
-				// 每50个deviceid去访问
-				if (allDeviceid.size() % 50 == 0) {
-					listDeviceid.add(strSize);
-					strSize = "";
-				} else {
-					strSize += t.getBsDeviceid() + ",";
-				}
-			}
+		String query="select bs_deviceid from vw_wxremind";
+		List<String> listDeviceid = systemService.findListbySql(query);
+		if(listDeviceid.size()>0)
+		{
+			System.out.println("执行电子围栏提醒消息开始...");
+			this.NoticePerson(listDeviceid);
+			System.out.println("执行电子围栏提醒消息结束...");
 		}
-		if (strSize != "")
-			listDeviceid.add(strSize);
-		// 访问并获取需要预警的值
-		List<String> noticeList = this.CompareRemoteTime(listDeviceid);
-		this.NoticePerson(noticeList);
-
-		System.out.println("执行电子围栏提醒消息结束...");
-	}
-
-	/**
-	 * 获取超过24小时需要预警的值
-	 * 
-	 * @param listDeviceid
-	 * @return
-	 */
-	private List<String> CompareRemoteTime(List<String> listDeviceid) {
-		// 需要提醒的设备
-		List<String> noticeList = new ArrayList<String>();
-
-		// 围栏经纬度,围栏默认30km
-		double lat = 0.0;
-		double lng = 0.0;
-		double distanct = 30.0;
-		List<BusConfigEntity> bceLat = systemService.findByProperty(BusConfigEntity.class, "cfCode", "MapLatitude");
-		List<BusConfigEntity> bceLong = systemService.findByProperty(BusConfigEntity.class, "cfCode", "MapLongitude");
-		List<BusConfigEntity> bceDistanct = systemService.findByProperty(BusConfigEntity.class, "cfCode",
-				"MapDistance");
-
-		if (bceLat.size() > 0)
-			lat = Double.parseDouble(bceLat.get(0).getCfValue());
-		if (bceLong.size() > 0)
-			lng = Double.parseDouble(bceLong.get(0).getCfValue());
-		if (bceDistanct.size() > 0)
-			distanct = Double.parseDouble(bceDistanct.get(0).getCfValue());
-
-		for (String deviceids : listDeviceid) {
-			System.out.println("开始执行:" + deviceids);
-			JSONObject json = new JSONObject();
-			List<Map<String, Object>> dataObject = new ArrayList<Map<String, Object>>();
-			String s1 = "RemoteUrlGets";
-			String s3 = "RemoteUrlAccessToken";
-			StringBuffer sql = new StringBuffer(
-					"SELECT bc.cf_code,bc.cf_name,bc.cf_value FROM bus_config bc where bc.cf_code='" + s1
-							+ "' or bc.cf_code='" + s3 + "'");
-			dataObject = this.systemService.findForJdbc(sql.toString());// this.systemService.findHql(hql.toString());
-			// 开始远程访问
-			String requestUrl = "";
-			RequestLocationsDevice rlsd = new RequestLocationsDevice();
-			rlsd.setDEVICEIDS(deviceids);
-			for (Map<String, Object> o : dataObject) {
-				if (s1.equals(o.get("cf_code").toString()))
-					requestUrl = o.get("cf_value").toString();
-				if (s3.equals(o.get("cf_code").toString()))
-					rlsd.setAccessToken(o.get("cf_value").toString());
-			}
-			System.out.println("访问->" + requestUrl);
-			if (requestUrl != "") {
-				JSONObject ob = JSONObject.fromObject(rlsd);
-				System.out.println(ob.toString());
-				json = HttpRequestPost.doPost(requestUrl, ob);
-				SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
-				for (String deviceid : deviceids.split(",")) {
-					if (!(deviceid == null || "".equals(deviceids)))
-						if (json.toString().contains(deviceid)) {
-							net.sf.json.JSONArray array = net.sf.json.JSONArray
-									.fromObject(json.get(deviceid).toString());// 将json字符串转成json数组
-							for (int i = 0; i < array.size(); i++) {// 循环json数组
-								JSONObject job = (JSONObject) array.get(i);// 得到json对象
-								// 时间转换
-								if (job.has("deviceId")) {
-									if (job.getString("type").equals("2")) {
-										try {
-											// 超过一天未获取数据，则报警
-											Date remoteTime = df.parse(job.getString("device_time"));
-											double[] clearLocation = ConvertionUtils.getClear(
-													job.getString("gps_latitude"), job.getString("gps_longitude"));
-											double distance = Distance.getDistance(clearLocation[1], clearLocation[0],
-													lng, lat);
-											System.out.println(job.getString("deviceId") + "距离：" + distance);
-											job.put("distance", distance);
-											if (distance > distanct) {
-												noticeList.add(job.toString());
-											}
-										} catch (Exception e) {
-											// TODO Auto-generated catch block
-											// e.printStackTrace();
-										}
-									}
-								}
-							}
-						}
-				}
-			}
-		}
-
-		return noticeList;
 	}
 
 	/*
@@ -181,22 +74,19 @@ public class wxsmsDistanceReminder implements Job {
 			String accessToken = wxutils.getAcctonken();
 			TemplateMessageSendResult msgSend = new TemplateMessageSendResult();
 			// 将信息保存到数据库表
-			for (String job : noticeList) {
-				JSONObject o = JSONObject.fromObject(job);
-				if (o.has("deviceId")) {
-					String deviceId = o.getString("deviceId");
+			for (String deviceId : noticeList) {
 					List<BasStudentInfoEntity> listBse = systemService.findByProperty(BasStudentInfoEntity.class,
 							"bsDeviceid", deviceId);
 					for (BasStudentInfoEntity t : listBse) {
 						if (t.getBsStatus().equals("Y")) {
-							System.out.println(job);
-							Map<String, TemplateData> data = new HashMap<String, TemplateData>();
-							data.put("first", new TemplateData("尊敬的家长，你的小孩超出电子围栏。", "#173177"));
-							data.put("keyword1",
-									new TemplateData(t.getBcGrade() + " " + t.getBcName(), "#FF0000"));
-							data.put("keyword2", new TemplateData(t.getBsName(), "#173177"));
-							data.put("keyword3", new TemplateData(o.getString("distance"), "#173177"));
 							try {
+								//通知家长
+								Map<String, TemplateData> data = new HashMap<String, TemplateData>();
+								data.put("first", new TemplateData("尊敬的家长，你的小孩在监控范围内，超出电子围栏。", "#173177"));
+								data.put("keyword1",
+										new TemplateData(t.getBcGrade() + " " + t.getBcName(), "#FF0000"));
+								data.put("keyword2", new TemplateData(t.getBsName(), "#173177"));
+								data.put("keyword3", new TemplateData("--", "#173177"));
 								List<BusOpenidEntity> listboe = systemService.findByProperty(BusOpenidEntity.class,
 										"bsStudentid", t.getId());
 								for(BusOpenidEntity boe :listboe)
@@ -205,12 +95,31 @@ public class wxsmsDistanceReminder implements Job {
 									msgSend.setTouser(boe.getBoOpenid());
 									msgSend.setData(data);
 									JwSendTemplateMsgAPI.sendTemplateMsg(accessToken, msgSend);	
+									systemService.executeSql("update bus_mapfence set is_send='Y' where device_id='"+deviceId+"'");
 								}
+								
+								//通知班主任
+								Map<String, TemplateData> dataTeacher = new HashMap<String, TemplateData>();
+								dataTeacher.put("first", new TemplateData("尊敬的班主任，你的学生在监控范围内，超出电子围栏。", "#173177"));
+								dataTeacher.put("keyword1",
+										new TemplateData(t.getBcGrade() + " " + t.getBcName(), "#FF0000"));
+								dataTeacher.put("keyword2", new TemplateData(t.getBsName(), "#173177"));
+								dataTeacher.put("keyword3", new TemplateData("--", "#173177"));
+								String query="SELECT tu.openid FROM t_s_base_user tu,bas_class bc,bas_student bs WHERE tu.id = bc.bc_personid and bc.id=bs.bc_id and bs.bs_deviceid='"+deviceId+"'";
+								List<String> listOpenid = systemService.findListbySql(query);
+								for(String s :listOpenid)
+								{
+									msgSend.setTemplate_id(basWXController.Templateid_notice);
+									msgSend.setTouser(s);
+									msgSend.setData(dataTeacher);
+									JwSendTemplateMsgAPI.sendTemplateMsg(accessToken, msgSend);	
+									systemService.executeSql("update bus_mapfence set is_send='Y' where device_id='"+deviceId+"'");
+								}
+								
 							} catch (WexinReqException e) {
 							}
 						}
 					}
-				}
 			}
 
 		} catch (WexinReqException e) { // TODO Auto-generated catch block //
